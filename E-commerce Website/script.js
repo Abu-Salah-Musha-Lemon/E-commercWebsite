a4fd9d5edd9a37aae7a752a213c71ea1cd452efa
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-02-02 22:13:37 +0600

    add javaScript file copy the nav bar to the mobile view

---
+++ E-commerce Website/script.js
@@ -0,0 +1,18 @@
+//coppy inside .dpt-cat ot .department
+
+function copyMenu() {
+    // copy inside .dpt-cat to .department
+    var dptCategory = document.querySelector(".dpt-cat")
+    var dptPlace = document.querySelector(".departments")
+    dptPlace.innerHTML = dptCategory.innerHTML
+    //copy inside nave to nav
+    var mainNav = document.querySelector(".header-nav nav")
+    var navPlace = document.querySelector(".off-canves nav")
+    navPlace.innerHTML = mainNav.innerHTML
+
+    // copy .header-nav .wrapper to .thetop-nav
+    var topNav = document.querySelector(".header-top")
+    var topPlase = document.querySelector(".off-canvas .thetop-nav")
+    topPlase.innerHTML= topNav.innerHTML;
+}
+copyMenu()
